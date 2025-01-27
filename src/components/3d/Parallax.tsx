3971a659d1375ed7099391af82081c5f000caef4
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-01-27 17:02:04 +0000

    up

--- src/components/3d/Parallax.tsx
+++ src/components/3d/Parallax.tsx
@@ -14,15 +14,13 @@ const Parallax = () => {
  
   ]
     return (
-      <section className="relative h-[500px] w-full overflow-hidden">
+      <section className="relative h-[500px] border-4 w-full overflow-hidden rounded-md">
         <div className="absolute inset-0 z-0">
         <ImageTransitionGallery imageUrls={IMAGE_URLS} />
         </div>
   
         <div className="relative z-10 h-full flex flex-col items-center justify-center text-center px-4">
-      <h1>
-        nklsnklnlklm,ksc,klkkkls
-      </h1>
+      <h1 className="text-3xl font-bold">text afficher</h1>
 
         </div>
       </section>
