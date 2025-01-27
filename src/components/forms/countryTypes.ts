a79976568c17be437eadaf44f5f51d6e768f3768
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-01-27 17:28:09 +0000

    up

--- src/components/forms/countryTypes.ts
+++ src/components/forms/countryTypes.ts
@@ -1,15 +1,14 @@
 import { Country } from 'country-state-city';
 
-// Obtenir la liste des pays
 const COUNTRIES = Country.getAllCountries();
 
-// Définir le type `SelectMenuOption` basé sur la structure des objets dans `COUNTRIES`
+
 export type SelectMenuOption = {
-  isoCode: string; // ISO code du pays
-  name: string; // Nom du pays
+  isoCode: string; 
+  name: string; 
 };
 
-// Mapper `COUNTRIES` pour correspondre à ce type
+
 export const countryOptions: SelectMenuOption[] = COUNTRIES.map(country => ({
   isoCode: country.isoCode,
   name: country.name,
