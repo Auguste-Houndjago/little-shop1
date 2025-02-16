2d96b151774ca2d500bbdaaa37ae5966a0c0ce87
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-02-16 10:13:46 +0000

    up

--- src/app/vendors/page.tsx
+++ src/app/vendors/page.tsx
@@ -69,7 +69,7 @@ const Page = async () => {
             <DollarSign className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">{formatPrice(totalRevenue)}</div>
+            <div className="text-sm md:text-2xl font-bold">{formatPrice(totalRevenue)}</div>
           </CardContent> 
         </Card>
         
